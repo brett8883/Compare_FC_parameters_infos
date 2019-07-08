72365dfc21ebba4d2fe3a024cfb0f0556e7fa012
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-07 22:29:24 -0700

    bug fix

--- compareSTOCK.bat
+++ compareSTOCK.bat
@@ -12,11 +12,11 @@ ECHO	[5] Phantom 4 Pro
 ECHo	[6] Phantom 4 Pro v2
 ECHO	[7] Inspire 2
 choice /C 1234567 /D 1 /T 99 /M "Please select factory parameters to compare"
-If Errorlevel 7 set facparms=got to sorry
+If Errorlevel 7 goto sorry
 If Errorlevel 6 set facparms=flcy_param_infos_p4pv2
 If Errorlevel 5 set facparms=flcy_param_infos_p4p
 If Errorlevel 4 set facparms=flyc_param_infos_P4A
-If Errorlevel 3 set facparms=goto sorry
+If Errorlevel 3 goto sorry
 If Errorlevel 2 set facparms=flcy_param_infos_spark
 If Errorlevel 1 set facparms=flcy_param_infos_mavicPro
 
@@ -37,7 +37,7 @@ exit
 
 
 :sorry
-csl 
+cls 
 ECHO -------------------------------------------------------------------------------------------
 ECHO Compare FC Parameters_infos
 echo By Brett8883
